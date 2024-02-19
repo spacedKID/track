4d846e041bb457f000c47a4d39bf1d0e2a16531d
spacedKID <ajwride@gmail.com>
2024-02-19 14:15:08 -0700

    add keyboard_listener

---
+++ src/keyboard_listener.py
@@ -0,0 +1,18 @@
+from pynput import keyboard
+
+def on_activate():
+    print("Shortcut CMD+OPTION+T activated")
+    # Here you will call your function to activate the GUI
+
+def for_canonical(f):
+    return lambda k: f(l.canonical(k))
+
+hotkey = keyboard.HotKey(
+    keyboard.HotKey.parse('<cmd>+<alt>+t'),
+    on_activate
+)
+
+with keyboard.Listener(
+        on_press=for_canonical(hotkey.press),
+        on_release=for_canonical(hotkey.release)) as l:
+    l.join()
